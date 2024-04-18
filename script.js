bf9ff44a9d050ada10c529befbbfc71acab89f91
karenchloec <karenchloec@users.noreply.github.com>
2024-04-18 17:12:37 -0500

    carousel
    faded, carousel image and div

--- script.js
+++ script.js
@@ -4,4 +4,22 @@ $(document).ready(function() {
             scrollTop: $('#jumbotron2').offset().top
         }, 1000);
     });
-});
+})
+
+    // Initialize carousel
+    $(document).ready(function() {
+        $('#scrollToNext').click(function(){
+            $('html, body').animate({
+                scrollTop: $('#jumbotron2').offset().top
+            }, 1000);
+        });
+    
+        // Initialize carousel with options
+        var myCarousel = document.querySelector('#fadingCarousel');
+        var carousel = new bootstrap.Carousel(myCarousel, {
+            interval: 3000,
+            wrap: true,
+            touch: true,
+            fade: true
+        });
+    });
