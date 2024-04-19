5e756057d25bd3b9ec57785022889e6e878202bb
karenchloec <karenchloec@users.noreply.github.com>
2024-04-18 21:25:28 -0500

    gifs
    responsive gif for each jumbotrons h1

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-$(document).ready(function() {
-    $('#scrollToNext').click(function(){
+$(document).ready(function () {
+    $('#scrollToNext').click(function () {
         $('html, body').animate({
             scrollTop: $('#jumbotron2').offset().top
         }, 1000);
     });
 })
 
-    // Initialize carousel
-    $(document).ready(function() {
-        $('#scrollToNext').click(function(){
-            $('html, body').animate({
-                scrollTop: $('#jumbotron2').offset().top
-            }, 1000);
-        });
-    
-        // Initialize carousel with options
-        var myCarousel = document.querySelector('#fadingCarousel');
-        var carousel = new bootstrap.Carousel(myCarousel, {
-            interval: 3000,
-            wrap: true,
-            touch: true,
-            fade: true
-        });
+$(document).ready(function () {
+    $('#scrollToNext').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#jumbotron2').offset().top
+        }, 1000);
     });
+
+    var myCarousel = document.querySelector('#fadingCarousel');
+    var carousel = new bootstrap.Carousel(myCarousel, {
+        interval: 3000,
+        wrap: true,
+        touch: true,
+        fade: true
+    });
+});
